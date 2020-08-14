8df840a7f032a127d2fb03c32eec4733006463b1
utokaz <49720451+utokaz@users.noreply.github.com>
2020-08-14 22:18:52 +0900

    Initial Commit

---
+++ BooksMakeYouYou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BooksMakeYouYou
+//
+//  Created by uto kazuori on 2020/08/14.
+//  Copyright © 2020 uto kazuori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
